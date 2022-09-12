edc8439b86cae8a5646ca53fac2711fdb392e63a
root <root@ip-172-31-33-9.ec2.internal>
2022-09-12 21:01:08 +0000

    Só um A

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,7 +10,7 @@ const Header = ({ title, onAdd, showAdd }) => {
       {location.pathname === "/" && (
         <Button
           color={showAdd ? "purple" : "purple"}
-          text={showAdd ? "CCClose" : "AAAdd"}
+          text={showAdd ? "Close" : "Add"}
           onClick={onAdd}
         />
       )}
